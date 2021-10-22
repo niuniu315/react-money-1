eb11ac63338c849a56b51d057922d4e351619121
niuniu315 <454378439@qq.com>
2021-10-22 22:47:01 +0800

    搞定三个icons

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,16 +2,29 @@ import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
 
+require('icons/money.svg');
+require('icons/label.svg');
+require('icons/statistics.svg');
+
+
 const NavWrapper = styled.nav`
   line-height: 24px;
-  box-shadow: 0 0 3px rgba(0,0,0,0.25);
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+
   > ul {
     display: flex;
 
     > li {
       width: 33.3%;
       text-align: center;
-      padding: 16px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      padding: 4px 0;
+      .icon{
+        width: 24px;
+        height: 24px;
+      }
     }
   }
 `;
@@ -21,12 +34,21 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#label"></use>
+          </svg>
           <Link to="/tags">标签页</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#money"></use>
+          </svg>
           <Link to="/money">记账页</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#statistics"></use>
+          </svg>
           <Link to="/statistics">统计页</Link>
         </li>
       </ul>
