bc4ae1058f6a048932a16c7aeb2d7bedb1b80bcd
niuniu315 <454378439@qq.com>
2021-10-22 16:46:15 +0800

    添加三个图标文件

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,71 @@
 import React from 'react';
 
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from 'react-router-dom';
+
+import styled from 'styled-components';
+import Nav from './components/Nav';
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+const Main = styled.div`
+  flex-grow: 1;
+  overflow: auto;
+`;
+
+
 function App() {
   return (
-    <div>
-    </div>
+    <Router>
+      <Wrapper>
+        <Main>
+          <Switch>
+            <Route path="/tags">
+              <Tags/>
+            </Route>
+            <Route path="/money">
+              <Money/>
+            </Route>
+            <Route path="/statistics">
+              <Statistics/>
+            </Route>
+            {/*  默认路由地址（初始地址）*/}
+            <Redirect exact from="/" to="/money"/>
+            {/* 404路由 */}
+            <Route path="*">
+              <NoMatch/>
+            </Route>
+          </Switch>
+        </Main>
+        <Nav/>
+      </Wrapper>
+    </Router>
   );
+}
+
+function NoMatch() {
+  return (
+    <div>路径不存在</div>
+  );
+}
+
+function Statistics() {
+  return <h2>统计页面</h2>;
+}
+
+function Tags() {
+  return <h2>标签页面</h2>;
+}
+
+function Money() {
+  return <h2>记账页面</h2>;
 }
 
 export default App;
