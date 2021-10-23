252cd8a67754838492fa849586149a18e34da2d6
niuniu315 <454378439@qq.com>
2021-10-23 16:42:36 +0800

    创建 Layout.tsx 组件

--- src/App.tsx
+++ src/App.tsx
@@ -9,22 +9,12 @@ import {
 
 import styled from 'styled-components';
 import Nav from './components/Nav';
-const Wrapper = styled.div`
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-`;
-const Main = styled.div`
-  flex-grow: 1;
-  overflow: auto;
-`;
+import Layout from './components/Layout';
 
 
 function App() {
   return (
     <Router>
-      <Wrapper>
-        <Main>
           <Switch>
             <Route path="/label">
               <Tags/>
@@ -42,9 +32,7 @@ function App() {
               <NoMatch/>
             </Route>
           </Switch>
-        </Main>
         <Nav/>
-      </Wrapper>
     </Router>
   );
 }
@@ -55,16 +43,28 @@ function NoMatch() {
   );
 }
 
-function Statistics() {
-  return <h2>统计页面</h2>;
-}
-
 function Tags() {
-  return <h2>标签页面</h2>;
+  return (
+    <Layout>
+      <h2>标签页面</h2>
+    </Layout>
+  );
 }
 
 function Money() {
-  return <h2>记账页面</h2>;
+  return (
+    <Layout>
+      <h2>记账页面</h2>
+    </Layout>
+  );
+}
+
+function Statistics() {
+  return (
+    <Layout>
+      <h2>统计页面</h2>
+    </Layout>
+  );
 }
 
 export default App;
