670ea540cd140dc0f1c0df1b174b71c2e6767346
niuniu315 <454378439@qq.com>
2021-10-24 01:06:25 +0800

    完成money 整体布局优化

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -4,6 +4,8 @@ import styled from 'styled-components';
 
 const TagsSection = styled.section`
   background: #FFFFFF; padding: 12px 16px;
+  flex-grow: 1; display:flex; flex-direction: column;
+  justify-content: flex-end; align-items: flex-start;
   > ol { margin: 0 -12px;
     > li{
        background: #D9D9D9; border-radius: 18px;
@@ -127,9 +129,14 @@ const NumberPadSection = styled.section`
   }
 `;
 
+const MyLayout = styled(Layout)`
+  display:flex;
+  flex-direction: column;
+`
+
 function Money() {
   return (
-    <Layout>
+    <MyLayout>
       <TagsSection>
         <ol>
           <li>衣</li>
@@ -172,7 +179,7 @@ function Money() {
           <button className="dot">.</button>
         </div>
       </NumberPadSection>
-    </Layout>
+    </MyLayout>
   );
 }
 
