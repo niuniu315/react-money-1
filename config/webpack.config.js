0f0cdcb583e6998d7174fd07cdba9ed6cd9d711a
niuniu315 <454378439@qq.com>
2021-10-24 00:43:51 +0800

    icon不显示

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -366,11 +366,13 @@ module.exports = function (webpackEnv) {
           // "oneOf" will traverse all following loaders until one will
           // match the requirements. When no loader matches it will fall
           // back to the "file" loader at the end of the loader list.
+
           oneOf: [
             {
               test: /\.svg$/,
               use: [
                 {loader: 'svg-sprite-loader', options: {}},
+
                 {
                   loader: 'svgo-loader', options: {
                     plugins: [
@@ -380,6 +382,7 @@ module.exports = function (webpackEnv) {
                 }
               ]
             },
+
             // TODO: Merge this config once `image/avif` is in the mime-db
             // https://github.com/jshttp/mime-db
             {
--- src/App.tsx
+++ src/App.tsx
@@ -4,29 +4,35 @@ import Money from './views/Money';
 import Statistics from './views/Statistics';
 import Tags from './views/Tags';
 import NoMatch from './views/NoMatch';
+import styled from 'styled-components';
 
+const AppWrapper = styled.div`
+  color: #333;
+`;
 
 function App() {
   return (
-    <Router>
-          <Switch>
-            <Route path="/label">
-              <Tags/>
-            </Route>
-            <Route path="/money">
-              <Money/>
-            </Route>
-            <Route path="/statistics">
-              <Statistics/>
-            </Route>
-            {/*  默认路由地址（初始地址）*/}
-            <Redirect exact from="/" to="/money"/>
-            {/* 404路由 */}
-            <Route path="*">
-              <NoMatch/>
-            </Route>
-          </Switch>
-    </Router>
+    <AppWrapper>
+      <Router>
+        <Switch>
+          <Route path="/label">
+            <Tags/>
+          </Route>
+          <Route path="/money">
+            <Money/>
+          </Route>
+          <Route path="/statistics">
+            <Statistics/>
+          </Route>
+          {/*  默认路由地址（初始地址）*/}
+          <Redirect exact from="/" to="/money"/>
+          {/* 404路由 */}
+          <Route path="*">
+            <NoMatch/>
+          </Route>
+        </Switch>
+      </Router>
+    </AppWrapper>
   );
 }
 
